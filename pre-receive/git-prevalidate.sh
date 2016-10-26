5ce6220bdb55ce04a8775b3af1390016348dc757
Ravi Julapalli <ravi.julapalli@eurostar.com>
2016-10-26 13:58:01 -0400

    Pre-receive hook complete with validation against a given API

--- pre-receive/git-prevalidate.sh
+++ pre-receive/git-prevalidate.sh
@@ -5,7 +5,7 @@ echo $from_ref
 echo $to_ref
 echo $ref_name
 fail=
-endpoint="http://www.google.com/"
+endpoint="https://github.com/ravijk/git-hooks/blob/master/README.md/"
 (
        	git rev-list --format=oneline $from_ref..$to_ref | sed 's/^/  /' |
 		while read commit_message 
@@ -14,19 +14,29 @@ endpoint="http://www.google.com/"
 			fail_message="Rejected due to following errors"
 			
 			## Validate if you can find reference to a pattern
-			if [[ $str_text =~ $commit_message ]]; then 
+			if [[ $commit_message =~ $pattern ]]; then 
 				item=${BASH_REMATCH[0]}
 				## We have a item, lets use API to validate this item
-				echo $item
-				url_with_item=endpoint"/"$item
-				echo url_with_item
+				url_with_item=$endpoint$item
+				echo $url_with_item
+				
+				status=$(curl -s --head $url_with_item | head -n 1 )
+				echo $status
+				if [[ $stauts =~ "200 OK" ]]; then 
+					echo "Work item :" $item " Validated = OK"
+					
+					#API returns 200 OK, don't do anything
+				else 
+					echo >&2 "Unable to validate item: " $item 
+					echo >&2 $url_with_item ." returns " .$status
+					exit -1
+				fi	
 			else 
 				fail=1
-				echo >&2 "******** Commits without reference to work item not permitted. *******" 
-				# Lets just print all revisions for user to review
-				echo >&2 ".. COMMITS .."
-				git rev-list --format=oneline $from_ref..$to_ref | sed 's/^/  /'
-				echo >&2 "**********************************************" 
+				echo >&2 "*** Commits without reference to work item not permitted. ***" 
+				echo >&2 $commit_message 
+				url_with_item=endpoint + $item
+				echo $url_with_item
 				exit -1
 				echo $fail
 			fi
